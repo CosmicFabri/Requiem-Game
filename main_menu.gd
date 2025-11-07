fcb4f3949aa7d8a1c47892e5b667eff4847417ae
EydenNav <150830498+EydenNav@users.noreply.github.com>
2025-11-06 19:21:54 -0600

    menufuncional
    solo funciona inicio y salir

---
+++ main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) 
+	
+func _on_start_pressed() -> void:
+	print("start pressed")
+	get_tree().change_scene_to_file("res://scenes/level_1/level_1.tscn")
+
+
+func _on_settings_pressed() -> void:
+	print("settings pressed") # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	print("exit pressed")# Replace with function body.
+	get_tree().quit()
--- scenes/level_1/level_1.gd
+++ scenes/level_1/level_1.gd
@@ -3,6 +3,7 @@ extends Node3D
 var score = 0
 @onready var score_label = %ScoreLabel
 
+
 func increase_score():
 	score += 1
 	score_label.text = "Score: " + str(score)
