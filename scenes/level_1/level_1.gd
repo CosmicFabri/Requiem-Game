f1bbc3084ab7b1910b223929099178a092e14c6e
CosmicFabri <fabrizzio86@outlook.es>
2025-11-09 09:23:37 -0600

    feat: Added level manager; next level mechanism working.

--- scenes/level_1/level_1.gd
+++ scenes/level_1/level_1.gd
@@ -3,6 +3,8 @@ extends Node3D
 var score = 0
 @onready var score_label = %ScoreLabel
 
+var mobs_spawned = 0
+
 func increase_score():
 	score += 1
 	score_label.text = "Score: " + str(score)
@@ -19,6 +21,11 @@ func _on_mob_spawner_3d_mob_spawned(mob):
 		do_poof(mob.global_position)
 	)
 	do_poof(mob.global_position)
+	mobs_spawned += 1
+	print(mobs_spawned)
+	
+	if mobs_spawned > 2:
+		LevelManager.next_level()
 
 func _on_kill_plane_body_entered(body):
 	get_tree().reload_current_scene.call_deferred()
